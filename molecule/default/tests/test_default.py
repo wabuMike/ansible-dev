14b1733367c8ea4b554879e249a4b46e862e224e
Michael Haak <mike_haak@protonmail.com>
2020-02-09 20:45:53 +0100

    Added molecule install task (to be tested!)

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -11,3 +11,8 @@ def test_ansible_installed(host):
     ansible = host.package('ansible')
 
     assert ansible.is_installed
+
+def test_virtualenv_installed(host):
+    packages = host.pip_package.get_packages(pip_path='~/molecule/bin/pip')
+
+    assert "molecule" in packages
